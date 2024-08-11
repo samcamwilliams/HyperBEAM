5a87534168e838faa644406233d21f95ee090cbf
Sam Williams <sam@arweave.org>
2024-08-11 17:22:36 -0400

    feat: Add compatible(?) API for getting assignment ranges.

--- src/su_http.erl
+++ src/su_http.erl
@@ -33,12 +33,38 @@ read_body(Req0, Acc) ->
         {more, Data, Req} -> read_body(Req, << Acc/binary, Data/binary >>)
     end.
 
+handle(<<"GET">>, [], Req, State) ->
+    Wallet = su_registry:get_wallet(),
+    cowboy_req:reply(200,
+        #{<<"Content-Type">> => <<"application/json">>},
+        jiffy:encode({[
+            {<<"Unit">>, <<"Scheduler">>},
+            {<<"Address">>, ar_util:encode(ar_wallet:to_address(Wallet))},
+            {<<"Timestamp">>, list_to_binary(integer_to_list(erlang:system_time(millisecond)))},
+            {<<"Processes">>, lists:map(fun ar_util:encode/1, su_registry:get_processes())}
+        ]}),
+        Req),
+    {ok, Req, State};
 handle(<<"GET">>, [ProcID, <<"slot">>], Req, State) ->
     CurrentSlot = su_process:get_current_slot(su_registry:find(binary_to_list(ProcID))),
     cowboy_req:reply(200,
         #{<<"Content-Type">> => <<"text/plain">>},
         integer_to_list(CurrentSlot),
         Req),
+    {ok, Req, State};
+handle(<<"GET">>, [BinProcID], Req, State) ->
+    ProcID = binary_to_list(BinProcID),
+    #{ to := To, from := From } =
+        cowboy_req:match_qs(
+            [
+                {from, [], "0"},
+                {to, [], binary_to_list(
+                    integer_to_binary(
+                        su_process:get_current_slot(
+                            su_registry:find(ProcID))))
+                }
+            ], Req),
+    send_stream(tn1, ProcID, From, To, Req),
     {ok, Req, State};
 handle(<<"GET">>, [ID], Req, State) when byte_size(ID) =:= 43 ->
     Message = su_data:read_message(ID),
@@ -53,7 +79,7 @@ handle(<<"POST">>, [], Req, State) ->
     true = ar_bundles:verify_item(Message),
     ProcID = su_registry:find(binary_to_list(ar_util:encode(Message#tx.target))),
     Assignment = su_process:schedule(ProcID, Message),
-    cowboy_req:reply(200,
+    cowboy_req:reply(201,
         #{<<"Content-Type">> => <<"application/json">>},
         jiffy:encode({[
             {id, ar_util:encode(Assignment#tx.id)},
@@ -64,3 +90,44 @@ handle(<<"POST">>, [], Req, State) ->
 
 allowed_methods(Req, State) ->
 	{[<<"GET">>, <<"POST">>], Req, State}.
+
+%% Private methods
+
+% Send existing-SU GraphQL compatible results
+send_stream(tn1, ProcID, From, To, Req) ->
+    cowboy_req:reply(200,
+        #{<<"Content-Type">> => <<"application/json">>},
+        jiffy:encode(
+            assignments_to_json(
+                su_process:get_assignments(
+                    ProcID,
+                    list_to_integer(From),
+                    list_to_integer(To))
+                )
+            ),
+        Req
+    ).
+
+assignments_to_json({Assignments, HasNextPage}) ->
+    {[
+        {<<"page_info">>,
+            {[
+                {<<"has_next_page">>, HasNextPage}
+            ]}
+        },
+        {<<"edges">>, [
+            {[
+                {<<"cursor">>, element(2, lists:keyfind(<<"Nonce">>, 1, Assignment#tx.tags))},
+                {<<"node">>, assignment_to_json(Assignment)}
+            ]}
+            || Assignment <- Assignments
+        ]}
+    ]}.
+
+assignment_to_json(Assignment) ->
+    {_, MessageID} = lists:keyfind(<<"Message">>, 1, Assignment#tx.tags),
+    Message = su_data:read_message(MessageID),
+    {[
+        {<<"message">>, ar_bundles:item_to_json_struct(Message)},
+        {<<"assignment">>, ar_bundles:item_to_json_struct(Assignment)}
+    ]}.
