97d395c8a09cfa53cb4c561b7bd2ed2c265b08b1
Sam Williams <sam@arweave.org>
2024-10-05 18:15:21 -0400

    Revert "Merge branch 'main' into wamr-mvp"
    This reverts commit 045a87266edc2ed3e33cfe54aa0e162558f8a233, reversing
    changes made to a25c0a53800e5d9d6b8e5ef6551ac4368c6bcd75.

--- src/ao.erl
+++ src/ao.erl
@@ -1,23 +1,61 @@
 -module(ao).
--export([config/0, get/1, c/1]).
+-export([config/0, get/1, get/2, c/1, c/2, build/0, profile/0]).
+-export([wallet/0, wallet/1]).
+
+-include("include/ar.hrl").
+
+wallet() -> wallet(ao:get(key_location)).
+wallet(Location) ->
+    case file:read_file_info(Location) of
+        {ok, _} -> ar_wallet:load_keyfile(Location);
+        {error, _} -> ar_wallet:new_keyfile(?DEFAULT_KEY_TYPE, ao:get(key_location))
+    end.
 
 config() ->
     #{
+        % Functional options
         http_port => 8734,
         http_host => "localhost",
         gateway => "https://arweave.net",
         bundler => "https://up.arweave.net",
         su => "http://localhost:8734/su",
         mu => "http://localhost:8734/mu",
-        cu => "https://cu24.ao-testnet.xyz",
+        cu => "http://localhost:8734/cu",
         key_location => "hyperbeam-key.json",
         default_page_limit => 5,
-        scheduler_location_ttl => 60 * 60 * 24 * 30
+        scheduler_location_ttl => 60 * 60 * 24 * 30,
+        preloaded_devices =>
+            #{
+                <<"Checkpoint">> => dev_checkpoint,
+                <<"Scheduler">> => dev_scheduler,
+                <<"Cron">> => dev_cron,
+                <<"Deduplicate">> => dev_dedup,
+                <<"JSON-Interface">> => dev_json_iface,
+                <<"Monitor">> => dev_monitor,
+                <<"WASM64-pure">> => dev_wasm
+            },
+        loadable_devices => [],
+        % Dev options
+        profiling => true
     }.
 
-get(Key) ->
-    maps:get(Key, config()).
+get(Key) -> get(Key, undefined).
+get(Key, Default) ->
+    maps:get(Key, config(), Default).
 
-c(X) ->
-    io:format("===== DEBUG PRINT =====~n~80p~n~n", [X]),
+c(X) -> c(X, "").
+c(X, ModAtom) when is_atom(ModAtom) -> c(X, "[" ++ atom_to_list(ModAtom) ++ "]");
+c(X, ModStr) ->
+    io:format(standard_error, "===== DEBUG PRINT~p =====~s> ~80p~n", [self(), ModStr, X]),
     X.
+
+build() ->
+    r3:do(compile, [{dir, "src"}]).
+
+profile() ->
+    case ao:get(profiling) of
+        false -> not_profiling;
+        true ->
+            eprof:stop_profiling(),
+            eprof:analyze(total)
+    end.
---
+++ src/cu_beamr_io.erl
@@ -0,0 +1,94 @@
+-module(cu_beamr_io).
+-export([size/1, read/3, write/3]).
+-export([read_string/2, read_iovecs/3, write_string/2]).
+-export([malloc/2, free/2]).
+
+-include("include/ao.hrl").
+
+size(Port) ->
+    Port ! {self(), {command, term_to_binary({size})}},
+    receive
+        {ok, Size} ->
+            {ok, Size};
+        Error ->
+            Error
+    end.
+
+write(Port, Offset, Data) ->
+    Port ! {self(), {command, term_to_binary({write, Offset, Data})}},
+    receive
+        ok ->
+            ok;
+        Error ->
+            Error
+    end.
+
+write_string(Port, Data) when is_list(Data) ->
+    write_string(Port, iolist_to_binary(Data));
+write_string(Port, Data) ->
+    DataSize = byte_size(Data) + 1,
+    String = <<Data/bitstring, 0:8>>,
+    case malloc(Port, DataSize) of
+        {ok, Ptr} ->
+            case write(Port, Ptr, String) of
+                ok ->
+                    {ok, Ptr};
+                Error ->
+                    free(Port, Ptr),
+                    Error
+            end;
+        Error ->
+            Error
+    end.
+
+read(Port, Offset, Size) ->
+    Port ! {self(), {command, term_to_binary({read, Offset, Size})}},
+    receive
+        {ok, Result} ->
+            {ok, Result};
+        Error ->
+            Error
+    end.
+
+read_string(Port, Offset) ->
+    {ok, iolist_to_binary(do_read_string(Port, Offset, 8))}.
+
+do_read_string(Port, Offset, ChunkSize) ->
+    {ok, Data} = read(Port, Offset, ChunkSize),
+    case binary:split(Data, [<<0>>]) of
+        [Data|[]] -> [Data|do_read_string(Port, Offset + ChunkSize, ChunkSize)];
+        [FinalData|_Remainder] -> [FinalData]
+    end.
+
+read_iovecs(Port, Ptr, Vecs) ->
+    %ao:c({read_iovecs_started, Port, Ptr, Vecs}),
+    Bin = iolist_to_binary(do_read_iovecs(Port, Ptr, Vecs)),
+    {ok, Bin}.
+
+do_read_iovecs(_Port, _Ptr, 0) -> [];
+do_read_iovecs(Port, Ptr, Vecs) ->
+    {ok, VecStruct} = read(Port, Ptr, 16),
+    <<BinPtr:64/little-unsigned-integer, Len:64/little-unsigned-integer>> = VecStruct,
+    {ok, VecData} = read(Port, BinPtr, Len),
+    [ VecData | do_read_iovecs(Port, Ptr + 16, Vecs - 1) ].
+
+malloc(Port, Size) ->
+    case cu_beamr:call(Port, "malloc", [Size]) of
+        {ok, [0]} ->
+            ao:c({malloc_failed, Size}),
+            {error, malloc_failed};
+        {ok, [Ptr]} ->
+            ao:c({malloc_success, Ptr, Size}),
+            {ok, Ptr};
+        {error, Error} ->
+            {error, Error}
+    end.
+
+free(Port, Ptr) ->
+    case cu_beamr:call(Port, "free", [Ptr]) of
+        {ok, Res} ->
+            ao:c({free_result, Res}),
+            ok;
+        {error, Error} ->
+            {error, Error}
+    end.
