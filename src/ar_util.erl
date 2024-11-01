b2c1fa5f7e449186ded8910912bbc4b4ba1b7027
Tom Wilson <tom@hyper.io>
2024-11-01 18:00:45 +0000

    fix(ar_util): fix a duplicate code issue as a result of a merge

--- src/ar_util.erl
+++ src/ar_util.erl
@@ -1,67 +1,69 @@
 -module(ar_util).
--export([id/1, encode/1, decode/1, safe_encode/1, safe_decode/1, find_value/2, find_value/3]).
+
+-export([id/1, encode/1, decode/1, safe_encode/1, safe_decode/1, find_value/2,
+         find_value/3]).
 -export([remove_common/2]).
+
 -include("include/ao.hrl").
 
 %% @doc Encode an ID in any format to a normalized, b64u 43 character binary.
 id(Bin) when is_binary(Bin) andalso byte_size(Bin) == 43 ->
-	Bin;
+  Bin;
 id(Bin) when is_binary(Bin) andalso byte_size(Bin) == 32 ->
-	b64fast:encode(Bin);
+  b64fast:encode(Bin);
 id(Data) when is_list(Data) ->
-	id(list_to_binary(Data)).
-
-%% @doc Encode an ID in any format to a normalized, b64u 43 character binary.
-id(Bin) when is_binary(Bin) andalso byte_size(Bin) == 43 ->
-	Bin;
-id(Bin) when is_binary(Bin) andalso byte_size(Bin) == 32 ->
-	b64fast:encode(Bin);
-id(Data) when is_list(Data) ->
-	id(list_to_binary(Data)).
+  id(list_to_binary(Data)).
 
 %% @doc Encode a binary to URL safe base64 binary string.
 encode(Bin) ->
-	b64fast:encode(Bin).
+  b64fast:encode(Bin).
 
 %% @doc Try to decode a URL safe base64 into a binary or throw an error when
 %% invalid.
 decode(Input) ->
-	b64fast:decode(Input).
+  b64fast:decode(Input).
 
 safe_encode(Bin) when is_binary(Bin) ->
-	encode(Bin);
+  encode(Bin);
 safe_encode(Bin) ->
-	Bin.
+  Bin.
 
 %% @doc Safely decode a URL safe base64 into a binary returning an ok or error
 %% tuple.
 safe_decode(E) ->
-	try
-		D = decode(E),
-		{ok, D}
-	catch
-		_:_ ->
-			{error, invalid}
-	end.
+  try
+    D = decode(E),
+    {ok, D}
+  catch
+    _:_ ->
+      {error, invalid}
+  end.
 
 %% @doc Find the value associated with a key in parsed a JSON structure list.
 find_value(Key, List) ->
-    ar_util:find_value(Key, List, undefined).
-find_value(Key, Map, Default) when is_map(Map) ->
-    case maps:find(Key, Map) of
-        {ok, Value} -> Value;
-        error -> Default
-    end;
-find_value(Key, List, Default) ->
-	case lists:keyfind(Key, 1, List) of
-		{Key, Val} -> Val;
-		false -> Default
-	end.
+  ar_util:find_value(Key, List, undefined).
 
-remove_common(<< X:8, Rest1/binary>>, << X:8, Rest2/binary>>) ->
-	?c({common_prefix, X}),
-    remove_common(Rest1, Rest2);
-remove_common([X|Rest1], [X|Rest2]) ->
-    remove_common(Rest1, Rest2);
-remove_common([$/|Path], _) -> Path;
-remove_common(Rest, _) -> Rest.
+find_value(Key, Map, Default) when is_map(Map) ->
+  case maps:find(Key, Map) of
+    {ok, Value} ->
+      Value;
+    error ->
+      Default
+  end;
+find_value(Key, List, Default) ->
+  case lists:keyfind(Key, 1, List) of
+    {Key, Val} ->
+      Val;
+    false ->
+      Default
+  end.
+
+remove_common(<<X:8, Rest1/binary>>, <<X:8, Rest2/binary>>) ->
+  ?c({common_prefix, X}),
+  remove_common(Rest1, Rest2);
+remove_common([X | Rest1], [X | Rest2]) ->
+  remove_common(Rest1, Rest2);
+remove_common([$/ | Path], _) ->
+  Path;
+remove_common(Rest, _) ->
+  Rest.
