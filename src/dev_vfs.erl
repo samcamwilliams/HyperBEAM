f489090a370b7864f9f532ac789d2acd6e16f1f2
Sam Williams <sam@arweave.org>
2024-12-23 08:54:57 +0500

    wip: draft vfs impl with Converge

--- src/dev_vfs.erl
+++ src/dev_vfs.erl
@@ -7,6 +7,7 @@
 
 -module(dev_vfs).
 -export([init/3, execute/3, stdout/1]).
+-export([path_open/3, fd_write/3, fd_read/3]).
 -include("include/hb.hrl").
 
 -define(INIT_VFS,
@@ -45,12 +46,8 @@ init(M1, _M2, Opts) ->
         hb_converge:set(
             M1,
             #{
-                <<"priv/WASM/stdlib/wasi_snapshot_preview1/path_open">> =>
-                    fun path_open/3,
-                <<"priv/WASM/stdlib/wasi_snapshot_preview1/fd_write">> =>
-                    fun fd_write/3,
-                <<"priv/WASM/stdlib/wasi_snapshot_preview1/fd_read">> =>
-                    fun fd_read/3
+                <<"WASM/stdlib/wasi_snapshot_preview1">> =>
+                    #{ device => <<"WASI-VFS/1.0">>}
             },
             Opts
         ),
@@ -71,165 +68,158 @@ init(M1, _M2, Opts) ->
     {ok, CompleteMsg}.
 
 %% @doc Encode the input message for inclusion in the VFS.
-execute(M1 = #{ pass := 1 }, M2, Opts) ->
-    MsgToProc = hb_converge:get(<<"Message">>, M2, Opts),
-    JSON =
-        ar_bundles:serialize(
-            hb_message:message_to_tx(MsgToProc),
-            json
-        ),
-    ?event(setting_stdin_to_message),
-    {ok,
-        hb_converge:set(
-            M1,
-            <<"vfs/dev/stdin">>,
-            JSON,
-            Opts
-        )
-    };
-execute(M1 = #{ pass := 2, vfs := FDs }, _M2, Opts) ->
-    {ok, 
-        hb_converge:set(
-            M1,
-            <<"vfs">>,
-            maps:from_list(
-                [
-                    {Filename, Data}
-                ||
-                    #{
-                        filename := Filename,
-                        data := Data
-                    } <- maps:values(FDs)
-                ]
-            ),
-            Opts
-        )
-    };
-execute(M1, _M2, _Opts) ->
-    {ok, M1}.
+execute(M1, M2, Opts) ->
+    case hb_converge:get(<<"Pass">>, M1, Opts) of
+        1 ->
+            MsgToProc = hb_converge:get(<<"Message">>, M2, Opts),
+            JSON =
+                ar_bundles:serialize(
+                    hb_message:message_to_tx(MsgToProc),
+                    json
+                ),
+            ?event(setting_message_vfs_key),
+            {ok,
+                hb_converge:set(
+                    M1,
+                    <<"vfs/message">>,
+                    JSON,
+                    Opts
+                )
+            };
+        _ -> {ok, M1}
+    end.
 
 %% @doc Return the stdout buffer from a state message.
 stdout(M) ->
-    hb_converge:get(<<"vfs/1">>, M).
+    hb_converge:get(<<"vfs/dev/stdout">>, M).
 
 %% @doc Adds a file descriptor to the state message.
-path_open(M, Port, [FDPtr, LookupFlag, PathPtr|_]) ->
-    FDs = hb_converge:get(<<"File-Descriptors">>, M),
+%path_open(M, Port, [FDPtr, LookupFlag, PathPtr|_]) ->
+path_open(Msg1, Msg2, Opts) ->
+    FDs = hb_converge:get(<<"File-Descriptors">>, Msg1, Opts),
+    Port = hb_private:get(<<"Port">>, Msg1, Opts),
+    [FDPtr, LookupFlag, PathPtr|_] = hb_converge:get(<<"Args">>, Msg2, Opts),
     ?event({path_open, FDPtr, LookupFlag, PathPtr}),
     Path = hb_beamr_io:read_string(Port, PathPtr),
     ?event({path_open, Path}),
-    #{
-        index := Index,
-        data := Data,
-        offset := Offset
+    FD = #{
+        index := Index
     } =
-        case hb_converge:get(<<"vfs/", Path/binary>>, M) of
+        case hb_converge:get(<<"vfs/", Path/binary>>, Msg1) of
             not_found ->
                 #{
-                    index => maps:size(FDs) + 1,
+                    index => length(hb_converge:keys(FDs)) + 1,
                     filename => Path,
-                    data => <<>>,
                     offset => 0
                 };
             F -> F
         end,
     {
-        hb_converge:set(
-            M,
-            <<"vfs/", Path/binary>>,
-            #{
-                index => Index,
-                data => Data,
-                offset => Offset
-            }
-        ),
-        [0, Index]
+        ok,
+        #{
+            state =>
+                hb_converge:set(
+                    Msg1,
+                    <<"vfs/", Path/binary>>,
+                    FD
+                ),
+            wasm_response => [0, Index]
+        }
     }.
 
 %% @doc WASM stdlib implementation of `fd_write', using the WASI-p1 standard
 %% interface.
-fd_write(S, Port, [FD, Ptr, Vecs, RetPtr]) ->
-    fd_write(S, Port, [FD, Ptr, Vecs, RetPtr], 0);
-fd_write(S, Port, Args) ->
-    ?event({fd_write, Port, Args}),
-    {S, [0]}.
-fd_write(S, Port, [_, _Ptr, 0, RetPtr], BytesWritten) ->
+fd_write(Msg1, Msg2, Opts) ->
+    State = hb_converge:get(<<"State">>, Msg1, Opts),
+    Port = hb_private:get(<<"WASM/Port">>, State, Opts),
+    [FD, Ptr, Vecs, RetPtr|_] = hb_converge:get(<<"Args">>, Msg2, Opts),
+    fd_write(State, Port, [FD, Ptr, Vecs, RetPtr], 0, Opts).
+
+fd_write(S, Port, [_, _Ptr, 0, RetPtr], BytesWritten, _Opts) ->
     hb_beamr_io:write(
         Port,
         RetPtr,
         <<BytesWritten:64/little-unsigned-integer>>
     ),
-    {S, [0]};
-fd_write(S, Port, [FD, Ptr, Vecs, RetPtr], BytesWritten) ->
-    FD = hb_converge:get(<<"File-Descriptors/", FD/binary>>, S),
-    Filename = hb_converge:get(<<"Filename">>, FD),
+    {ok, #{ state => S, wasm_response => [0] }};
+fd_write(S, Port, [FDnum, Ptr, Vecs, RetPtr], BytesWritten, Opts) ->
+    FDNumStr = integer_to_binary(FDnum),
+    FD = hb_converge:get(<<"File-Descriptors/", FDNumStr/binary>>, S, Opts),
+    Filename = hb_converge:get(<<"Filename">>, FD, Opts),
+    StartOffset = hb_converge:get(<<"Offset">>, FD, Opts),
     {VecPtr, Len} = parse_iovec(Port, Ptr),
     {ok, Data} = hb_beamr_io:read(Port, VecPtr, Len),
     Before =
         binary:part(
-            OrigData = hb_converge:get(<<"Data">>, FD),
+            OrigData = hb_converge:get(<<"Data">>, FD, Opts),
             0,
-            hb_converge:get(<<"Offset">>, FD)
+            StartOffset
         ),
     After =
-        binary:part(
-            OrigData,
-            hb_converge:get(<<"offset">>, FD),
-            byte_size(OrigData) - hb_converge:get(<<"offset">>, FD)
-        ),
-    NewFile =
+        binary:part(OrigData, StartOffset, byte_size(OrigData) - StartOffset),
+    S1 =
         hb_converge:set(
-            FD,
-            #{
-                <<"data">> => <<Before/binary, Data/binary, After/binary>>,
-                <<"offset">> =>
-                    hb_converge:get(<<"offset">>, FD) + byte_size(Data)
-            }
+            S,
+            <<"File-Descriptors/", FDNumStr/binary, "/Offset">>,
+            StartOffset + byte_size(Data),
+            Opts
+        ),
+    S2 =
+        hb_converge:set(
+            S1,
+            <<"vfs/", Filename/binary>>,
+            <<Before/binary, Data/binary, After/binary>>,
+            Opts
         ),
     fd_write(
-        hb_converge:set(S, <<"vfs/", Filename/binary>>, NewFile),
+        S2,
         Port,
         [FD, Ptr + 16, Vecs - 1, RetPtr],
-        BytesWritten + byte_size(Data)
+        BytesWritten + byte_size(Data),
+        Opts
     ).
 
 %% @doc Read from a file using the WASI-p1 standard interface.
-fd_read(S, Port, Args) ->
-    fd_read(S, Port, Args, 0).
-fd_read(S, Port, [FD, _VecsPtr, 0, RetPtr], BytesRead) ->
+fd_read(Msg1, Msg2, Opts) ->
+    State = hb_converge:get(<<"State">>, Msg1, Opts),
+    Port = hb_private:get(<<"WASM/Port">>, State, Opts),
+    [FD, VecsPtr, NumVecs, RetPtr|_] = hb_converge:get(<<"Args">>, Msg2, Opts),
+    fd_read(State, Port, [FD, VecsPtr, NumVecs, RetPtr], 0, Opts).
+
+fd_read(S, Port, [FD, _VecsPtr, 0, RetPtr], BytesRead, _Opts) ->
     ?event({{completed_read, FD, BytesRead}}),
     hb_beamr_io:write(Port, RetPtr,
         <<BytesRead:64/little-unsigned-integer>>),
-    {S, [0]};
-fd_read(S, Port, [FD, VecsPtr, NumVecs, RetPtr], BytesRead) ->
-    ?event({fd_read, FD, VecsPtr, NumVecs, RetPtr}),
-    FD = hb_converge:get(<<"File-Descriptors/", FD/binary>>, S),
-    Filename = hb_converge:get(<<"Filename">>, FD),
+    {ok, #{ state => S, wasm_response => [0] }};
+fd_read(S, Port, [FDNum, VecsPtr, NumVecs, RetPtr], BytesRead, Opts) ->
+    ?event({fd_read, FDNum, VecsPtr, NumVecs, RetPtr}),
+    % Parse the request
+    FDNumStr = integer_to_binary(FDNum),
+    Filename =
+        hb_converge:get(
+            <<"File-Descriptors/", FDNumStr/binary, "/Filename">>, S, Opts),
     {VecPtr, Len} = parse_iovec(Port, VecsPtr),
-    {FileBytes, NewFile} = get_bytes(FD, Len),
-    ok = hb_beamr_io:write(Port, VecPtr, FileBytes),
+    % Read the bytes from the file
+    Data = hb_converge:get(<<"vfs/", Filename/binary>>, S, Opts),
+    Offset =
+        hb_converge:get(
+            <<"File-Descriptors/", FDNumStr/binary, "/Offset">>, S, Opts),
+    ReadSize = min(Len, byte_size(Data) - Offset),
+    Bin = binary:part(Data, Offset, ReadSize),
+    % Write the bytes to the WASM port
+    ok = hb_beamr_io:write(Port, VecPtr, Bin),
     fd_read(
-        hb_converge:set(S, <<"vfs/", Filename/binary>>, NewFile),
+        hb_converge:set(
+            S,
+            <<"File-Descriptors/", FDNumStr/binary, "/Offset">>,
+            Offset + ReadSize,
+            Opts
+        ),
         Port,
-        [FD, VecsPtr + 16, NumVecs - 1, RetPtr],
-        BytesRead + byte_size(FileBytes)
+        [FDNum, VecsPtr + 16, NumVecs - 1, RetPtr],
+        BytesRead + ReadSize,
+        Opts
     ).
-
-%% @doc Find a file in the VFS from its name or file descriptor.
-get_file(M, Filename) when is_binary(Filename) ->
-    hb_converge:get(<<"vfs/", Filename/binary>>, M);
-get_file(M, FD) when is_integer(FD) ->
-    hb_converge:get(<<"File-Descriptors/", FD/binary>>, M).
-
-%% @doc Fetch bytes from the file, and update the file message with the new
-%% offset.
-get_bytes(FileMsg, Size) ->
-    Data = hb_converge:get(<<"data">>, FileMsg),
-    Offset = hb_converge:get(<<"offset">>, FileMsg),
-    AvailableSize = min(Size, byte_size(Data) - Offset),
-    Bin = binary:part(Data, Offset, AvailableSize),
-    {Bin, hb_converge:set(FileMsg, <<"offset">>, Offset + byte_size(Bin))}.
-
 
 %% @doc Parse an iovec in WASI-preview-1 format.
 parse_iovec(Port, Ptr) ->
