0ff9127c85e9f10c94c80defea0a34fd1f579173
Sam Williams <sam@arweave.org>
2024-12-27 00:10:36 +0500

    feat: add process_worker tests

--- src/dev_process_worker.erl
+++ src/dev_process_worker.erl
@@ -4,32 +4,72 @@
 %%% to computation requests regarding a process as a singleton.
 -module(dev_process_worker).
 -export([server/3, stop/1, group/3]).
+-include_lib("include/hb.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %% @doc Returns a group name for a request. The worker is responsible for all
 %% computation work on the same process on a single node, so we use the
 %% process ID as the group name.
-group(Msg1, _Msg2, Opts) ->
-    Proc = hb_converge:get(<<"Process">>, {as, dev_message, Msg1}, Opts),
-    hb_util:human_id(hb_converge:get(id, Proc, Opts#{ hashpath := ignore})).
+group(Msg1, undefined, Opts) ->
+    hb_persistent:default_grouper(Msg1, undefined, Opts);
+group(Msg1, Msg2, Opts) ->
+    case dev_message:get(path, Msg2, Opts) of
+        {ok, <<"Compute">>} ->
+            ID = process_to_group_name(Msg1, Opts),
+            ?event({id, ID}),
+            ID;
+        {_, OtherRes} ->
+            hb_persistent:default_grouper(Msg1, Msg2, Opts)
+    end.
+
+process_to_group_name(Msg1, Opts) ->
+    hb_util:human_id(
+        hb_converge:get(
+            id,
+            {as,
+                dev_message,
+                dev_process:ensure_process_key(Msg1, Opts)
+            },
+            Opts#{ hashpath => ignore}
+        )
+    ).
 
 %% @doc Spawn a new worker process. This is called after the end of the first
 %% execution of `hb_converge:resolve/3', so the state we are given is the
 %% already current.
-server(Msg1, _FirstMsg, Opts) ->
+server(GroupName, Msg1, Opts) ->
+    %hb_persistent:default_worker(GroupName, Msg1, Opts#{ static_worker => true }).
     receive
-        {resolve, Listener, Msg1, Msg2, ListenerOpts} ->
-            Res =
-                hb_converge:resolve(
-                    Msg1,
-                    Msg2,
-                    ListenerOpts#{ spawn_worker => false }
-                ),
-            Listener ! {resolved, self(), Msg1, Msg2, Res},
-            server(Msg1, Msg2, Opts);
-        stop -> ok
+        Req ->
+            ?event({req, Req}),
+            ok
     end.
 
 %% @doc Stop a worker process.
 stop(Worker) ->
     exit(Worker, normal).
 
+%%% Tests
+
+test_init() ->
+    application:ensure_all_started(hb),
+    ok.
+
+info_test() ->
+    test_init(),
+    M1 = dev_process:test_wasm_process(<<"test/aos-2-pure-xs.wasm">>),
+    Res = hb_converge:info(M1, #{}),
+    ?assertEqual(fun dev_process_worker:group/3, maps:get(grouper, Res)).
+
+grouper_test() ->
+    test_init(),
+    M1 = dev_process:test_aos_process(),
+    M2 = #{ path => <<"Compute">>, v => 1 },
+    M3 = #{ path => <<"Compute">>, v => 2 },
+    M4 = #{ path => <<"Not-Compute">>, v => 3 },
+    G1 = hb_persistent:group(M1, M2, #{}),
+    G2 = hb_persistent:group(M1, M3, #{}),
+    G3 = hb_persistent:group(M1, M4, #{}),
+    ?event({group_samples, {g1, G1}, {g2, G2}, {g3, G3}}),
+    ?assertEqual(G1, G2),
+    ?assertNotEqual(G1, G3).
