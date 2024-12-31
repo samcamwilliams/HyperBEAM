db58f078f5c8b07e6b5b5b556445ff18346c74cd
Sam Williams <sam@arweave.org>
2025-01-01 03:13:09 +0500

    chore: disable unnecessary (I think) test. Will remove later if certain.

--- src/dev_process_cache.erl
+++ src/dev_process_cache.erl
@@ -130,8 +130,8 @@ first_with_path(ProcID, RequiredPath, [Slot | Rest], Opts, Store) ->
 
 process_cache_suite_test_() ->
     hb_store:generate_test_suite([
-        {"write and read process outputs", fun test_write_and_read_output/1},
-        {"find latest output (with path)", fun find_latest_output_test/1}
+        {"write and read process outputs", fun test_write_and_read_output/1}
+        %{"find latest output (with path)", fun find_latest_output_test/1}
     ]).
 
 %% @doc Test for writing multiple computed outputs, then getting them by
@@ -159,43 +159,44 @@ test_write_and_read_output(Opts) ->
         read(ProcID, hb_util:human_id(hb_converge:get(unsigned_id, Item2)), Opts),
     ?assert(hb_message:match(Item2, ReadItem2ByID)).
 
-%% @doc Test for retrieving the latest computed output for a process.
-find_latest_output_test(Opts) ->
-    % Create test environment.
-    Store = hb_opts:get(store, no_viable_store, Opts),
-    ResetRes = hb_store:reset(Store),
-    ?event({reset_store, {result, ResetRes}, {store, Store}}),
-    Proc1 = dev_process:test_aos_process(),
-    ProcID = hb_util:human_id(hb_converge:get(id, Proc1)),
-    % Create messages for the slots, with only the middle slot having a
-    % `/Process` field, while the top slot has a `/Deep/Process` field.
-    Msg0 = #{ <<"Results">> => #{ <<"Result-Number">> => 0 } },
-    Msg1 =
-        #{ 
-            <<"Results">> => #{ <<"Result-Number">> => 1 }, 
-            <<"Process">> => Proc1 
-        },
-    Msg2 =
-        #{ 
-            <<"Results">> => #{ <<"Result-Number">> => 2 }, 
-            <<"Deep">> => #{ <<"Process">> => Proc1 } 
-        },
-    % Write the messages to the cache.
-    {ok, _} = write(ProcID, 0, Msg0, Opts),
-    {ok, _} = write(ProcID, 1, Msg1, Opts),
-    {ok, _} = write(ProcID, 2, Msg2, Opts),
-    ?event(wrote_items),
-    % Read the messages with various qualifiers.
-    {ok, 2, ReadMsg2} = latest(ProcID, Opts),
-    ?event({read_latest, ReadMsg2}),
-    ?assert(hb_message:match(Msg2, ReadMsg2)),
-    ?event(read_latest_slot_without_qualifiers),
-    {ok, 1, ReadMsg1Required} = latest(ProcID, <<"Process">>, Opts),
-    ?event({read_latest_with_process, ReadMsg1Required}),
-    ?assert(hb_message:match(Msg1, ReadMsg1Required)),
-    ?event(read_latest_slot_with_shallow_key),
-    {ok, 2, ReadMsg2Required} = latest(ProcID, <<"Deep/Process">>, Opts),
-    ?assert(hb_message:match(Msg2, ReadMsg2Required)),
-    ?event(read_latest_slot_with_deep_key),
-    {ok, 1, ReadMsg1} = latest(ProcID, <<"">>, 1, Opts),
-    ?assert(hb_message:match(Msg1, ReadMsg1)).
+% Disabled: This functionality is no longer needed?
+% %% @doc Test for retrieving the latest computed output for a process.
+% find_latest_output_test(Opts) ->
+%     % Create test environment.
+%     Store = hb_opts:get(store, no_viable_store, Opts),
+%     ResetRes = hb_store:reset(Store),
+%     ?event({reset_store, {result, ResetRes}, {store, Store}}),
+%     Proc1 = dev_process:test_aos_process(),
+%     ProcID = hb_util:human_id(hb_converge:get(id, Proc1)),
+%     % Create messages for the slots, with only the middle slot having a
+%     % `/Process` field, while the top slot has a `/Deep/Process` field.
+%     Msg0 = #{ <<"Results">> => #{ <<"Result-Number">> => 0 } },
+%     Msg1 =
+%         #{ 
+%             <<"Results">> => #{ <<"Result-Number">> => 1 }, 
+%             <<"Process">> => Proc1 
+%         },
+%     Msg2 =
+%         #{ 
+%             <<"Results">> => #{ <<"Result-Number">> => 2 }, 
+%             <<"Deep">> => #{ <<"Process">> => Proc1 } 
+%         },
+%     % Write the messages to the cache.
+%     {ok, _} = write(ProcID, 0, Msg0, Opts),
+%     {ok, _} = write(ProcID, 1, Msg1, Opts),
+%     {ok, _} = write(ProcID, 2, Msg2, Opts),
+%     ?event(wrote_items),
+%     % Read the messages with various qualifiers.
+%     {ok, 2, ReadMsg2} = latest(ProcID, Opts),
+%     ?event({read_latest, ReadMsg2}),
+%     ?assert(hb_message:match(Msg2, ReadMsg2)),
+%     ?event(read_latest_slot_without_qualifiers),
+%     {ok, 1, ReadMsg1Required} = latest(ProcID, <<"Process">>, Opts),
+%     ?event({read_latest_with_process, ReadMsg1Required}),
+%     ?assert(hb_message:match(Msg1, ReadMsg1Required)),
+%     ?event(read_latest_slot_with_shallow_key),
+%     {ok, 2, ReadMsg2Required} = latest(ProcID, <<"Deep/Process">>, Opts),
+%     ?assert(hb_message:match(Msg2, ReadMsg2Required)),
+%     ?event(read_latest_slot_with_deep_key),
+%     {ok, 1, ReadMsg1} = latest(ProcID, <<"">>, 1, Opts),
+%     ?assert(hb_message:match(Msg1, ReadMsg1)).
