fb7df6c4f2099e1eb75abc97d26751e2572cb6e8
Sam Williams <sam@arweave.org>
2025-01-20 01:26:12 -0500

    fix: 2 hb_http tests

--- src/hb_http.erl
+++ src/hb_http.erl
@@ -376,7 +376,7 @@ nested_converge_resolve_test() ->
     ?assertEqual(<<"Value2">>, hb_converge:get(<<"body">>, Res, #{})).
 
 wasm_compute_request(ImageFile, Func, Params) ->
-    wasm_compute_request(ImageFile, Func, Params, <<"results/output">>).
+    wasm_compute_request(ImageFile, Func, Params, <<"/results">>).
 wasm_compute_request(ImageFile, Func, Params, ResultPath) ->
     {ok, Bin} = file:read_file(ImageFile),
     #{
@@ -392,12 +392,14 @@ run_wasm_unsigned_test() ->
     Msg = wasm_compute_request(<<"test/test-64.wasm">>, <<"fac">>, [3.0]),
     {ok, Res} = post(Node, Msg, #{}),
     ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})).
+    ?assertEqual([6.0], hb_converge:get(<<"output">>, Res, #{})).
 
 run_wasm_signed_test() ->
     URL = hb_http_server:start_test_node(#{force_signed => true}),
     Msg = wasm_compute_request(<<"test/test-64.wasm">>, <<"fac">>, [3.0]),
     {ok, Res} = post(URL, Msg, #{}),
     ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})).
+    ?assertEqual([6.0], hb_converge:get(<<"output">>, Res, #{})).
 
 get_deep_unsigned_wasm_state_test() ->
     URL = hb_http_server:start_test_node(#{force_signed => false}),
