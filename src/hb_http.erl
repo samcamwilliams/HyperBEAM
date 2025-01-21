00b47162581d61c6b2d25c28eb29d2e2e063d4d1
Oleg <oltarasenko@gmail.com>
2025-01-21 16:22:31 +0100

    fix: fix hb_http typo

--- src/hb_http.erl
+++ src/hb_http.erl
@@ -391,14 +391,14 @@ run_wasm_unsigned_test() ->
     Node = hb_http_server:start_test_node(#{force_signed => false}),
     Msg = wasm_compute_request(<<"test/test-64.wasm">>, <<"fac">>, [3.0]),
     {ok, Res} = post(Node, Msg, #{}),
-    ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})).
+    ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})),
     ?assertEqual([6.0], hb_converge:get(<<"output">>, Res, #{})).
 
 run_wasm_signed_test() ->
     URL = hb_http_server:start_test_node(#{force_signed => true}),
     Msg = wasm_compute_request(<<"test/test-64.wasm">>, <<"fac">>, [3.0]),
     {ok, Res} = post(URL, Msg, #{}),
-    ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})).
+    ?assertEqual([6.0], hb_converge:get(<<"body">>, Res, #{})),
     ?assertEqual([6.0], hb_converge:get(<<"output">>, Res, #{})).
 
 get_deep_unsigned_wasm_state_test() ->
