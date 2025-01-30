63f82d0244b0b8800e37f14eba9413f5118a9364
Sam Williams <sam@arweave.org>
2025-01-30 13:20:51 -0500

    feat: end-to-end wasm payments test via HTTP

--- src/hb_examples.erl
+++ src/hb_examples.erl
@@ -59,3 +59,55 @@ relay_with_payments_test() ->
     {ok, Resp} = Res2,
     ?assert(length(hb_message:signers(Resp)) > 0),
     ?assert(hb_message:verify(Resp)).
+
+%% @doc Gain signed WASM responses from a node and verify them.
+%% 1. Start the client with a small balance.
+%% 2. Execute a simple WASM function on the host node.
+%% 3. Verify the response is correct and signed by the host node.
+%% 4. Get the balance of the client and verify it has been deducted.
+paid_wasm_test() ->
+    HostWallet = ar_wallet:new(),
+    ClientWallet = ar_wallet:new(),
+    ClientAddress = hb_util:human_id(ar_wallet:to_address(ClientWallet)),
+    ProcessorMsg =
+        #{
+            <<"device">> => <<"p4@1.0">>,
+            <<"ledger_device">> => <<"simple-pay@1.0">>,
+            <<"pricing_device">> => <<"simple-pay@1.0">>
+        },
+    HostNode =
+        hb_http_server:start_test_node(
+            #{
+                simple_pay_ledger => #{ ClientAddress => 100 },
+                simple_pay_price => 10,
+                operator => ar_wallet:to_address(HostWallet),
+                preprocessor => ProcessorMsg,
+                postprocessor => ProcessorMsg
+            }
+        ),
+    % Read the WASM file from disk, post it to the host and execute it.
+    {ok, WASMFile} = file:read_file(<<"test/test-64.wasm">>),
+    ClientMessage1 =
+        hb_message:attest(
+            #{
+                <<"path">> =>
+                    <<"/~wasm-64@1.0/init/compute/results?wasm-function=fac">>,
+                <<"body">> => WASMFile,
+                <<"wasm-params+list">> => <<"3.0">>
+            },
+            ClientWallet
+        ),
+    {ok, Res} = hb_http:post(HostNode, ClientMessage1, #{}),
+    % Check that the message is signed by the host node.
+    ?assert(length(hb_message:signers(Res)) > 0),
+    ?assert(hb_message:verify(Res)),
+    % Now we have the results, we can verify them.
+    ?assertMatch(6.0, hb_converge:get(<<"output/1">>, Res, #{})),
+    % Check that the client's balance has been deducted.
+    ClientMessage2 =
+        hb_message:attest(
+            #{<<"path">> => <<"/~simple-pay@1.0/balance">>},
+            ClientWallet
+        ),
+    {ok, Res2} = hb_http:get(HostNode, ClientMessage2, #{}),
+    ?assertMatch(20, hb_converge:get(<<"body">>, Res2, #{})).
