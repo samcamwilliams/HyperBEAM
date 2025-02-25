c601aef07edeabbfb2fe64408e3a923c6e8b99b3
Peter Farber <farberpete@gmail.com>
2025-02-25 11:43:46 -0700

    feat: add the default route to point to an index.html

--- src/hb_http_server.erl
+++ src/hb_http_server.erl
@@ -133,6 +133,7 @@ new_server(RawNodeMsg) ->
             [
                 % {HostMatch, list({PathMatch, Handler, InitialState})}
                 {'_', [
+                    {"/", cowboy_static, {priv_file, hb, "index.html"}},
                     {
                         "/metrics/[:registry]",
                         prometheus_cowboy2_handler,
