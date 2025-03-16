ea617d7d73d1cc94219e2b237cd43dabdb628dcd
Sam Williams <sam@arweave.org>
2025-03-16 18:38:15 -0400

    chore: abstract compute flow and bubble up errors

--- src/dev_genesis_wasm.erl
+++ src/dev_genesis_wasm.erl
@@ -13,9 +13,13 @@ init(Msg, _Msg2, _Opts) -> {ok, Msg}.
 %% the `patch@1.0` device, applying any state patches that the AO process may have
 %% requested.
 compute(Msg, Msg2, Opts) ->
-    {ok, Msg3} = hb_converge:resolve(Msg, {as, <<"delegated-compute@1.0">>, Msg2}, Opts),
-    {ok, Msg4} = hb_converge:resolve(Msg3, {as, <<"patch@1.0">>, Msg2}, Opts),
-    {ok, Msg4}.
+    case hb_converge:resolve(Msg, {as, <<"delegated-compute@1.0">>, Msg2}, Opts) of
+        {ok, Msg3} ->
+            {ok, Msg4} = hb_converge:resolve(Msg3, {as, <<"patch@1.0">>, Msg2}, Opts),
+            {ok, Msg4};
+        {error, Error} ->
+            {error, Error}
+    end.
 
 %% @doc Normalize the device.
 normalize(Msg, _Msg2, _Opts) -> {ok, Msg}.
