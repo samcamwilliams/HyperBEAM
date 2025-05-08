9799df32333c25d33a47332ebd120908381d2f32
dpshade <dylan.shade@arweave.org>
2025-05-08 13:39:22 -0400

    fix: revert to incoming change from main

--- src/dev_snp.erl
+++ src/dev_snp.erl
@@ -105,7 +105,7 @@ verify(M1, M2, NodeOpts) ->
     NonceMatches = report_data_matches(Address, NodeMsgID, Nonce),
     ?event({nonce_matches, NonceMatches}),
     % Step 2: Verify the address and the signature.
-    Signers = hb_message:signers(M2),
+    Signers = hb_message:signers(MsgWithJSONReport),
     ?event({snp_signers, {explicit, Signers}}),
     SigIsValid = hb_message:verify(MsgWithJSONReport, Signers),
     ?event({snp_sig_is_valid, SigIsValid}),
