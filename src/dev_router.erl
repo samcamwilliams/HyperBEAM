fae389ef54fced72db523c0277ae1bb38857e78e
Peter Farber <farberpete@gmail.com>
2025-06-06 18:45:32 +0200

    test: greenzone

--- src/dev_router.erl
+++ src/dev_router.erl
@@ -86,13 +86,11 @@ register(_M1, _M2, Opts) ->
         %% Generate attestation for secure node validation
         %% This proves the node's identity to the router
         Attestion = 
-            hb_cache:ensure_all_loaded(
-                hb_message:commit(
-                    hb_util:ok(dev_snp:generate(#{}, #{}, Opts)),
-                    Opts
-                ),
+            hb_message:commit(
+                hb_util:ok(dev_snp:generate(#{}, #{}, Opts)),
                 Opts
             ),
+ 
         ?event(debug_register, {attestion, Attestion}),
         %% Validate that all required parameters are present
         %% This will return {error, Reason} if any parameter is missing or invalid
@@ -105,28 +103,21 @@ register(_M1, _M2, Opts) ->
         %% Post registration request to the router node
         %% The message includes our route details and attestation for verification
         {ok, _} = hb_http:post(
-            RouterNode, 
-            #{
-                <<"path">> => <<"/router~node-process@1.0/schedule">>,
-                <<"method">> => <<"POST">>,
-                <<"body">> =>
-                    hb_cache:ensure_all_loaded(
-                        hb_message:commit(
-                            #{
-                                <<"path">> => <<"register">>,
-                                <<"route">> =>
-                                #{
-                                    <<"prefix">> => Prefix,
-                                    <<"template">> => Template,
-                                    <<"price">> => Price
-                                },
-                                <<"body">> => Attestion
-                            },
-                            Opts
-                        ),
-                        Opts
-                    )
-            },
+            RouterNode,
+            <<"/router~node-process@1.0/schedule">>,
+            hb_message:commit(
+                #{
+                    <<"action">> => <<"register">>,
+                    <<"route">> =>
+                    #{
+                        <<"prefix">> => Prefix,
+                        <<"template">> => Template,
+                        <<"price">> => Price
+                    },
+                    <<"body">> => Attestion
+                },
+                Opts
+            ),
             Opts
         ),
         {ok, <<"Route registered.">>}
