968e46459a2e17722fbd2bb48b58497f913e97f6
Sam Williams <sam@arweave.org>
2025-06-23 15:32:45 -0400

    chore: expose trace generation and formatting infra

--- src/hb_util.erl
+++ src/hb_util.erl
@@ -16,8 +16,8 @@
 -export([format_indented/2, format_indented/3, format_indented/4, format_binary/1]).
 -export([format_maybe_multiline/3, remove_trailing_noise/2]).
 -export([debug_print/4, debug_fmt/1, debug_fmt/2, debug_fmt/3, eunit_print/2]).
--export([print_trace/4, trace_macro_helper/5, print_trace_short/4]).
--export([format_trace/1, format_trace_short/0, format_trace_short/1]).
+-export([get_trace/0, print_trace/4, trace_macro_helper/5, print_trace_short/4]).
+-export([format_trace/1, trace_to_list/1, format_trace_short/0, format_trace_short/1]).
 -export([is_hb_module/1, is_hb_module/2, all_hb_modules/0]).
 -export([ok/1, ok/2, until/1, until/2, until/3]).
 -export([count/2, mean/1, stddev/1, variance/1, weighted_random/1]).
@@ -788,17 +788,21 @@ print_trace_short(Trace, Mod, Func, Line) ->
         ]
     ).
 
+%% @doc Return a list of calling modules and lines from a trace.
+trace_to_list(Trace) ->
+    lists:reverse(format_trace_short(
+        hb_opts:get(short_trace_len, 6, #{}),
+        false,
+        Trace,
+        hb_opts:get(stack_print_prefixes, [], #{})
+    )).
+
 %% @doc Format a trace to a short string.
 format_trace_short() -> format_trace_short(get_trace()).
 format_trace_short(Trace) -> 
     lists:join(
         " / ",
-        lists:reverse(format_trace_short(
-            hb_opts:get(short_trace_len, 6, #{}),
-            false,
-            Trace,
-            hb_opts:get(stack_print_prefixes, [], #{})
-        ))
+        trace_to_list(Trace)
     ).
 format_trace_short(_Max, _Latch, [], _Prefixes) -> [];
 format_trace_short(0, _Latch, _Trace, _Prefixes) -> [];
